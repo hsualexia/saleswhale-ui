fd6acd89909857d120a425e2c828f897d69fdbed
alexia <alexia.hsuey@gmail.com>
2022-05-18 21:58:21 +0800

    updated left menu to be customised instead of ant library

--- src/common/LeftMenuBar/index.js
+++ src/common/LeftMenuBar/index.js
@@ -1,33 +1,38 @@
 import React from "react";
+import { Row } from 'antd';
+
 import {
+  Logo,
   CampaignIcon,
   TeamsIcon,
   LeadsIcon,
   ReportsIcon,
   HelpIcon,
 } from "../../asset/img";
-import { SideBar, MenuStyled, HelpIconStyled } from "./styled";
-
-const getItem = (key, icon) => {
-  return { key, icon };
-};
-const items = [
-  getItem("1", <CampaignIcon />),
-  getItem("2", <TeamsIcon />),
-  getItem("3", <LeadsIcon />),
-  getItem("4", <ReportsIcon />)
-];
+import { SideBar, LinkStyled, LogoStyled } from "./styled";
 
 const LeftMenuBar = () => {
   return <SideBar>
-    <MenuStyled
-      mode="inline"
-      theme="dark"
-      items={items}
-    />
-    <HelpIconStyled>
+    <Row>
+      <LogoStyled>
+        <Logo />
+      </LogoStyled>
+      <LinkStyled>
+        <CampaignIcon />
+      </LinkStyled>
+      <LinkStyled>
+        <TeamsIcon />
+      </LinkStyled>
+      <LinkStyled>
+        <LeadsIcon />
+      </LinkStyled>
+      <LinkStyled>
+        <ReportsIcon />
+      </LinkStyled>
+    </Row>
+    <LinkStyled>
       <HelpIcon />
-    </HelpIconStyled>
+    </LinkStyled>
   </SideBar>
 };
 
--- src/common/LeftMenuBar/styled.js
+++ src/common/LeftMenuBar/styled.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { Menu } from 'antd';
 
 export const SideBar = styled.div`
   display: flex;
@@ -8,18 +7,38 @@ export const SideBar = styled.div`
   height: 100vh;
   background: #042235;
   justify-content: space-between;
+  align-items: center;
 `;
 
-export const MenuStyled = styled(Menu)`
-  &.ant-menu.ant-menu-dark {
-    background: #042235;
+export const LinkStyled = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding: 24px 0px;
+
+  :hover {
+    cursor: pointer;
+    background: #2995DA;
   }
 
-  & .ant-menu-item {
-    margin: 30px 0px;
+  :active {
+    background: #2995DA;
   }
 `;
 
-export const HelpIconStyled = styled.div`
-  padding: 20px 24px;
+export const LogoStyled = styled.div`
+  margin: 20px;
 `;
+// export const MenuStyled = styled(Menu)`
+//   &.ant-menu.ant-menu-dark {
+//     background: #042235;
+//   }
+
+//   & .ant-menu-item {
+//     margin: 30px 0px;
+//   }
+
+//   &.ant-menu-dark.ant-menu-dark:not(.ant-menu-horizontal) .ant-menu-item-selected {
+//     background-color: #042235;
+//   }
+// `;
